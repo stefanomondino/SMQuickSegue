13c90da6ca9cb95ad299b9a003a8952aecf98c61
Stefano Mondino <puntoste@gmail.com>
2014-01-27 09:36:56 +0100

    add/remove resources

---
+++ SMQuickSegueExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SMQuickSegue
+//
+//  Created by Stefano Mondino on 07/12/13.
+//  Copyright (c) 2013 Stefano Mondino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SMAppDelegate class]));
+    }
+}
